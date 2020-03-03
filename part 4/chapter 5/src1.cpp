515871c8b6383bf1294c6791db53439ec8535814
loginmen <38717842+loginmen@users.noreply.github.com>
2020-03-03 13:27:56 +0300

    Add files via upload

---
+++ part 4/chapter 5/src1.cpp
@@ -0,0 +1,10 @@
+float quadVertices[] = {  
+    // positions   // texCoords
+    -1.0f,  1.0f,  0.0f, 1.0f,
+    -1.0f, -1.0f,  0.0f, 0.0f,
+     1.0f, -1.0f,  1.0f, 0.0f,
+
+    -1.0f,  1.0f,  0.0f, 1.0f,
+     1.0f, -1.0f,  1.0f, 0.0f,
+     1.0f,  1.0f,  1.0f, 1.0f
+};	
